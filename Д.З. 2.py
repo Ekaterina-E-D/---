d99274d1b8edb95ba1bab3dcca4090ec17dfdb14
Ekaterina-E-D <158331151+Ekaterina-E-D@users.noreply.github.com>
2024-02-07 00:12:16 +0400

    Add files via upload

---
+++ Д.З. 2.py
@@ -0,0 +1,10 @@
+a1 = "Ardipithecus"
+a2 = "Australopithecus"
+a3 = "Paranthropus"
+a4 = "Homo habilis"
+a5 = "Homo erectus"
+a6 = "Homo neanderthalensis"
+a7 = "Homo sapiens"
+a8 = "Homo sapiens sapiens"
+print('Стадии развития человека')
+print(a1, a2, a3, a4, a5, a6, a7, a8, sep="=>")
